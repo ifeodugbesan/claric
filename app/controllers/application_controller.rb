562ffa0a944fe800b2b7aa8e7ba234e8cde673be
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-06-04 14:29:07 +0100

    finished locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
+   before_action :set_locale
   # app/controllers/application_controller.rb
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+
+  private
+
+   def set_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+  end
+
 end
