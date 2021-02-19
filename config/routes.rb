d787135a194441b593e8273b88931323582c71c4
gonzalodiaz <gonzalo@hoxton-digital.com>
2021-02-19 07:33:11 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get '/servicios', to: 'pages#servicios'
     get '/clientes', to: 'pages#clientes'
     get '/contactenos', to: 'pages#contactenos'
+
     get 'download_pdf', to: "pages#download_pdf"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
